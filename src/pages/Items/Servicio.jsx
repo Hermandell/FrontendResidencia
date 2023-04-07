022c6c4c1e7eb25b9a43ccbc699d7f9950b8c5ae
PcCristy <pepeslzr117@gmail.com>
2023-04-07 15:31:48 -0600

    formatea y elimina un console log

--- src/pages/Items/Servicio.jsx
+++ src/pages/Items/Servicio.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Servicio = ({ imagen, titulo, descripcion }) => {
-  console.log(imagen);
     return (
       <div className="col-md-6 col-lg-4">
         <div className="card border-0">
--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -28,29 +28,27 @@ const Servicios = () => {
     fetchData();
   }, []);
   servicios.map(item => {
-    console.log('http://localhost:1337'+item.attributes.img.data.attributes.formats.large.url)
+    console.log('http://localhost:1337' + item.attributes.img.data.attributes.formats.large.url)
     console.log(item.attributes.img.data.attributes.formats.large.url);
   })
   return (
     <>
-        <main className="page projects-page">
-
-            <section className="portfolio-block projects-cards">
-
-    <div className="container">
-      <div className="row">
-      {isLoading && <Loading />}
-      {!isLoading && servicios.map(servicio => (
-        <Servicio
-          key={servicio.id}
-          imagen={`http://localhost:1337${servicio.attributes.img.data.attributes.formats.large.url}`}
-          titulo={servicio.attributes.titulo}
-          descripcion={servicio.attributes.descripcion}
-        />
-      ))}
-      </div>
-      </div>
-      </section>
+      <main className="page projects-page">
+        <section className="portfolio-block projects-cards">
+          <div className="container">
+            <div className="row">
+              {isLoading && <Loading />}
+              {!isLoading && servicios.map(servicio => (
+                <Servicio
+                  key={servicio.id}
+                  imagen={`http://localhost:1337${servicio.attributes.img.data.attributes.formats.large.url}`}
+                  titulo={servicio.attributes.titulo}
+                  descripcion={servicio.attributes.tiempo}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
