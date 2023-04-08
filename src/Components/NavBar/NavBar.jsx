a522aaf6dffe4667e49c6474f4e045cd19ba15e8
PcCristy <pepeslzr117@gmail.com>
2023-04-08 08:13:27 -0600

    acomodo de carrito

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -44,16 +44,6 @@ const NavBar = () => {
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/cv" className="nav-link">
-                Contacto
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/nosotros" className="nav-link">
-                Nosotros
-              </Link>
-            </li>
-            <li className="nav-item">
               <button className="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#cartDropdown" aria-expanded="false">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
@@ -84,6 +74,16 @@ const NavBar = () => {
                   <button className="btn btn-primary mt-3">Pagar</button>
                 </div>
               </div>
+            </li>
+            <li className="nav-item">
+              <Link to="/cv" className="nav-link">
+                Contacto
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/nosotros" className="nav-link">
+                Nosotros
+              </Link>
             </li>
           </ul>
         </div>
